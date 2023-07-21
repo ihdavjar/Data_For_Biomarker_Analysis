436c750ded1b2ed441360563a44cb0e07c644bb1
vadhi raj <95899338+ihdavjar@users.noreply.github.com>
2023-07-22 00:00:57 +0530

    Add files via upload

---
+++ Lefse_Analysis/Phylum_Wise/bioconductor.R
@@ -0,0 +1,43 @@
+
+library(lefser)
+
+org_data<-read.csv("complete_phylum.csv")
+temp_cols_names<-names(org_data)
+
+temp_cols_names<-temp_cols_names[5:length(temp_cols_names)-1]
+print(temp_cols_names)
+
+temp_rows_names<-org_data$X
+print(temp_rows_names)
+
+col_data<-org_data[1:nrow(org_data),c(5:ncol(org_data)-1)]
+print(colnames(col_data))
+
+print(length(temp_cols_names))
+temp_assay=t(col_data)
+rownames(temp_assay)<-temp_cols_names
+colnames(temp_assay)<-temp_rows_names
+
+print(dim(temp_assay))
+temp_treatment<-org_data$treatment
+
+colData <- DataFrame(Treatment=temp_treatment,
+                     row.names=temp_rows_names)
+
+print(length(temp_rows_names))
+
+print(temp_treatment)
+temp_se<-SummarizedExperiment(assays=list(temp_assay),
+                     colData=colData)
+
+## -----------------------------------------------------------------------------
+res <- lefser(temp_se, groupCol = "Treatment",lda.threshold = 0)
+head(res)
+
+
+## -----------------------------------------------------------------------------
+lefserPlot(res, colors = c("blue","red"),trim.names = TRUE)
+
+## -----------------------------------------------------------------------------
+
+write.csv(res,"output_phylum.csv")
